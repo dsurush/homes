a7cca0127558d794120c23a7a165c8a3dd469f66
Surush <davlatzoda.surush@gmail.com>
2020-01-18 17:25:46 +0500

    fixed #2

--- main.go
+++ main.go
@@ -12,60 +12,71 @@ type home struct {
 	region string
 }
 
-func SortByPriceAsc(homes []home) []home {
+func SortBy(homes []home, less func(a, b home) bool) []home{
 	result := make([]home, len(homes))
 	copy(result, homes)
 	sort.Slice(result, func(i, j int) bool {
-		return result[i].price < result[j].price
+		return less(result[i], result[j])
 	})
 	return result
+}
+
+func SortByPriceAsc(homes []home) []home {
+	return SortBy(homes, func(a, b home) bool {
+		if a.price < b.price {
+			return true
+		}
+		return false
+	})
 }
 
 func SortByPriceDesc(homes []home) []home {
-	result := make([]home, len(homes))
-	copy(result, homes)
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].price > result[j].price
+	return SortBy(homes, func(a, b home) bool {
+		if a.price > b.price{
+			return true
+		}
+		return false
 	})
-	return result
 }
 
 func SortByDistanceFromCenterAsc(homes []home) []home {
-	result := make([]home, len(homes))
-	copy(result, homes)
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].distanceFromCenter < result[j].distanceFromCenter
+	return SortBy(homes, func(a, b home) bool {
+		return a.distanceFromCenter < b.distanceFromCenter
 	})
-	return result
 }
 
 func SortByDistanceFromCenterDesc(homes []home) []home {
-	result := make([]home, len(homes))
-	copy(result, homes)
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].distanceFromCenter > result[j].distanceFromCenter
+	return SortBy(homes, func(a, b home) bool {
+		return a.distanceFromCenter > b.distanceFromCenter
 	})
+}
+
+func FilterBy(homes []home, less func(a home) bool) []home{
+	result := make([]home, 0)
+	for _, home := range homes{
+		if less(home){
+			result = append(result, home)
+		}
+	}
 	return result
 }
 
 func SearchByMaxPrice(homes []home, maxPrice int64) []home{
-	result := make([]home, 0)
-	for _, home := range homes{
-		if home.price <= maxPrice{
-			result = append(result, home)
+	return FilterBy(homes, func(a home) bool {
+		if a.price <= maxPrice{
+			return true
 		}
-	}
-	return result
+		return false
+	})
 }
 
 func SearchFromXPriceToYPrice(homes []home, minPrice, maxPrice int64) []home{
-	result := make([]home, 0)
-	for _, home := range homes{
-		if home.price >= minPrice && home.price <= maxPrice{
-			result = append(result, home)
+	return FilterBy(homes, func(a home) bool {
+		if a.price >= minPrice && a.price <= maxPrice{
+			return true
 		}
-	}
-	return result
+		return false
+	})
 }
 
 func SearchByRegion(homes []home, regions []string) []home{
